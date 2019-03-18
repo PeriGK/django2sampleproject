df7d094c034571b9a5644eb35a91291c81b1ae5e
tsadimas <tsadimas@hua.gr>
2019-03-18 17:06:16 +0200

    create post

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:postid>/', views.detail, name='detail'),
+    path('new/', views.post_form,name='create')
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 
 from .models import Posts
+from .forms import PostsForm
 
 
-@login_required
 def index(request):
     data = Posts.objects.all()
-    context = {'data' : data}
+    context = {'data': data}
     return render(request, 'posts/index.html', context)
+
 
 def detail(request, postid):
     post = Posts.objects.get(pk=postid)
     context = {'post': post}
     return render(request, 'posts/detail.html', context)
+
+
+@login_required
+def post_form(request):
+    if request.method == 'GET':
+        form = PostsForm()
+    else:
+        form = PostsForm(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            body = form.cleaned_data['body']
+            post = Posts.objects.create(title=title, body=body, userId=request.user)
+            return HttpResponseRedirect("/posts/"+str(post.id))
+    return render(request, 'posts/newpost.html', {'form': form, 'url_redirect': 'posts'})
