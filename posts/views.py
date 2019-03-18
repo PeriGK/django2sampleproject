6d20c2450730f445791cd5a115be5bdcee778186
tsadimas <tsadimas@hua.gr>
2019-03-18 16:08:01 +0200

    added django-registration-redux

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 from .models import Posts
 
 
-
+@login_required
 def index(request):
     data = Posts.objects.all()
     context = {'data' : data}
