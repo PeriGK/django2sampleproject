6be81d98960a1c0871592bd60ad8220d0bf0aa10
tsadimas <tsadimas@hua.gr>
2019-03-18 16:33:17 +0200

    added post detail

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:postid>/', views.detail, name='detail'),
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -10,3 +10,8 @@ def index(request):
     data = Posts.objects.all()
     context = {'data' : data}
     return render(request, 'posts/index.html', context)
+
+def detail(request, postid):
+    post = Posts.objects.get(pk=postid)
+    context = {'post': post}
+    return render(request, 'posts/detail.html', context)
